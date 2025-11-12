151d80b485925da652acc7507d31f7e0d2348816
Lucas Torres <torres.lck@gmail.com>
2025-11-12 00:58:12 -0300

    implementação das classes Smartphone, Nokia e Iphone.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string Imei { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -22,5 +28,10 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public void Infos()
+        {
+            Console.WriteLine($"numero: {Numero}, Modelo: {Modelo}, Imei: {Imei}, Memoria: {Memoria}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 1", imei: "1", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.Infos();
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "789", modelo: "Iphone 1", imei: "2", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.Infos();
